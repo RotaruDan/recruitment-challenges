bcb6758802b9324a17c3da73057ab86e2bb9eac3
RotaruDan <drotaru@ucm.es>
2019-08-23 04:25:12 +0200

    Adds Fraudradar implementation. Tests now use the new class

--- fraud-detection/FraudRadar.test.js
+++ fraud-detection/FraudRadar.test.js
@@ -1,16 +1,20 @@
-const FraudRadar = require('./FraudRadar')
-const path = require('path')
+const FraudRadar = require('./FraudRadar/index')
+const FileHandler = require('./FraudRadar/FileHandler')
 const assert = require('assert')
+
 
 describe('Fraud Radar', function () {
   it('Should process the one line file', function () {
-    let result = FraudRadar.Check(path.join(__dirname, 'Files', 'OneLineFile.txt'))
+
+    let oneLineFileHandler = new FileHandler(['Files', 'OneLineFile.txt'])
+    let result = FraudRadar.Check(oneLineFileHandler)
     assert.ok(result)
     assert.equal(result.length, 0)
   })
 
   it('Should process the two line file in which the second is fraudulent', function () {
-    let result = FraudRadar.Check(path.join(__dirname, 'Files', 'TwoLines_FraudulentSecond.txt'))
+    let twoLineFileHandler = new FileHandler(['Files', 'TwoLines_FraudulentSecond.txt'])
+    let result = FraudRadar.Check(twoLineFileHandler)
     assert.ok(result)
     assert.equal(result.length, 1)
     assert.equal(result[0].isFraudulent, true)
@@ -18,7 +22,8 @@ describe('Fraud Radar', function () {
   })
 
   it('Should process the three line file in which the second is fraudulent', function () {
-    let result = FraudRadar.Check(path.join(__dirname, 'Files', 'ThreeLines_FraudulentSecond.txt'))
+    let threeLineFileHandler = new FileHandler(['Files', 'ThreeLines_FraudulentSecond.txt'])
+    let result = FraudRadar.Check(threeLineFileHandler)
     assert.ok(result)
     assert.equal(result.length, 1)
     assert.equal(result[0].isFraudulent, true)
@@ -26,7 +31,8 @@ describe('Fraud Radar', function () {
   })
 
   it('Should process the four line file in which more than one order is fraudulent', function () {
-    let result = FraudRadar.Check(path.join(__dirname, 'Files', 'FourLines_MoreThanOneFraudulent.txt'))
+    let fourLineFileHandler = new FileHandler(['Files', 'FourLines_MoreThanOneFraudulent.txt'])
+    let result = FraudRadar.Check(fourLineFileHandler)
     assert.ok(result)
     assert.equal(result.length, 2)
   })
